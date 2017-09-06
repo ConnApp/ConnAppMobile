d9ba8feaf168d0fac4f20abab89873abc80a0eeb
yannunes <yannunes@gmail.com>
2017-09-06 15:03:07 -0300

    Fix navigation styles and add navigation to Events screen

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,18 +1,30 @@
 import { StackNavigator } from 'react-navigation'
 import LaunchScreen from '../Containers/LaunchScreen'
+import Events from '../Containers/Events'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LaunchScreen: { screen: LaunchScreen }
+  LaunchScreen: {
+    screen: LaunchScreen,
+  },
+  Events: {
+    screen: Events,
+    navigationOptions: {
+      title: 'Programação'
+    }
+  },
+
 }, {
   // Default config for all screens
-  headerMode: 'none',
   initialRouteName: 'LaunchScreen',
   navigationOptions: {
-    headerStyle: styles.header
-  }
+    headerStyle: styles.header,
+    headerTintColor: 'white',
+  },
+  elevation: 0,
+  headerMode: 'screen',
 })
 
 export default PrimaryNav
