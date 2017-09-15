19387d50a372f3b61d921ccbb996b684e9610559
yannunes <yannunes@gmail.com>
2017-09-15 16:13:28 -0300

    Better organize events list handling and add mongo db listeners to update/insert events and locals

--- App/Containers/Events.js
+++ App/Containers/Events.js
@@ -25,48 +25,85 @@ export default class Events extends Component {
     }
   }
 
+  insertLocal (local) {
+    this.locals = [...this.locals, local]
+
+    this.setNewEventsState()
+  }
+
+  updateLocal (newLocal) {
+    this.locals = this.locals.map(local => {
+      if (local._id == newLocal._id) {
+        local = newLocal
+      }
+      return local
+    })
+
+    this.setNewEventsState()
+  }
+
+  insertEvent (event) {
+    this.events = [...this.events, event]
+
+    this.setNewEventsState()
+  }
+
+  updateEvent (newEvent) {
+    this.events = this.events.map(event => {
+      if (event.key.split(' - ')[1] == newEvent.local) {
+        event.data = event.data.map(ev => {
+          if (ev._id == newEvent._id) {
+            ev = newEvent
+          }
+          return ev
+        })
+      }
+      return event
+    })
+
+    this.setNewEventsState()
+  }
+
   componentWillMount() {
-    const dateQuery = this.getTodayFilter()
 
-    mongo.db.events.on('insert', event => {
-      console.log('event inserted')
-    })
+    mongo.db.events.on('insert',this.insertEvent)
 
-    mongo.db.events.on('update', event => {
-      console.log('event updated')
-    })
+    mongo.db.locals.on('insert', this.insertLocal)
 
-    mongo.db.locals.on('insert', local => {
-      console.log('local inserted')
-    })
+    mongo.db.events.on('update', this.updateEvent)
 
-    mongo.db.locals.on('update', local => {
-      console.log('local updated')
-    })
+    mongo.db.locals.on('update', this.updateLocal)
 
-    mongo.db.events.find({dateQuery})
+    mongo.db.events.find({ dateQuery: this.getTodayFilter() })
       .then(dbEvents => {
         this.events = [...dbEvents]
         return mongo.db.locals.find({})
       })
       .then(locals => {
-        locals = locals.reduce((localHashTable, local) => {
-          localHashTable[local._id] = local.name
-          return localHashTable
-        }, {})
-
-        this.events = this.events.map(event => {
-          event.local = locals[event.local]
-          return event
-        })
-
-        this.groupByLocal(this.events)
+        this.locals = [...locals]
+        this.setNewEventsState()
       })
       .catch(err => {
         // console.log(err)
       })
   }
 
+  setNewEventsState () {
+    this.locals = this.mapLocalToId(this.locals)
+    this.events = this.events.map(event => {
+      event.local = this.locals[event.local]
+      return event
+    })
+
+    this.groupEventsByLocal()
+  }
+
+  mapLocalToId (locals) {
+    return locals.reduce((localHashTable, local) => {
+      localHashTable[local._id] = `${local.name} - ${local._id}`
+      return localHashTable
+    }, {})
+  }
 
   getTodayFilter() {
     const day = parseInt(this.props.navigation.state.key.split(' ')[1])
@@ -88,11 +125,7 @@ export default class Events extends Component {
     return (sectionA < sectionB) ? -1 : (sectionA > sectionB) ? 1 : 0;
   }
 
-  groupByLocal(
-    dbEvents,
-    sectionSort = this.sortByRoom,
-    sortEvents = this.sortByStart
-  ) {
+  groupEventsByLocal(sectionSort = this.sortByRoom, sortEvents = this.sortByStart ) {
 
     let eventsArray =
       groupBy(this.events, 'local')
