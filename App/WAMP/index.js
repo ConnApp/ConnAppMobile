e09082669063f53de3a1b5689c7dc42a68672e74
yannunes <yannunes@gmail.com>
2017-09-16 16:55:54 -0300

    Check if there is Internet connections before using the websocket

--- App/WAMP/index.js
+++ App/WAMP/index.js
@@ -1,67 +1,74 @@
 import wampConfig from './config.js'
+import { NetInfo } from 'react-native'
 import wamp from 'wamp.js2'
 
 class WAMP {
   constructor({subArray = [], pubArray = []}) {
-    // Builds conenction object
-    this.Connection = new wamp.Connection(wampConfig)
+    NetInfo.isConnected.fetch().then(isConnected => {
+      console.log(`Internet is ${isConnected? 'Connected' : 'Disconnected'}`)
+      if (isConnected) {
+        // Builds conenction object
+        this.Connection = new wamp.Connection(wampConfig)
 
-    //Listens for status change to updat the class variables
-    this.Connection.onstatuschange(this.onstatuschange)
+        //Listens for status change to updat the class variables
+        this.Connection.onstatuschange(this.onstatuschange)
 
-    // Open connection handler
-    this.Connection.onopen = (session) => {
-      // If array subs exists, subscribe to each
-      // console.log('connection open')
-      if (subArray.length) {
-        this.subStats = subArray.map(sub => {
-          let status = true
-          try {
-            session.subscribe(sub.uri, sub.cb)
-          } catch(e) {
-            status = e
+        // Open connection handler
+        this.Connection.onopen = (session) => {
+          // If array subs exists, subscribe to each
+          // console.log('connection open')
+          if (subArray.length) {
+            this.subStats = subArray.map(sub => {
+              let status = true
+              try {
+                session.subscribe(sub.uri, sub.cb)
+              } catch(e) {
+                status = e
+              }
+              // console.log(`Subscribed to ${sub.uri}`)
+              return {
+                sub: sub,
+                status: status
+              }
+            })
           }
-          // console.log(`Subscribed to ${sub.uri}`)
-          return {
-            sub: sub,
-            status: status
+
+          // If array subs exists, subscribe to each
+          // console.log(pubArray.length)
+          if (pubArray.length) {
+            this.pubStats = pubArray.map(pub => {
+              let status = true
+
+              if (!Array.isArray(pub.data)) {
+                pub.data = [pub.data]
+              }
+
+              try {
+                session.publish(pub.uri, [pub.data], {data: pub.data})
+              } catch(e) {
+                status = e
+              }
+
+              // console.log(`Published ${pub.data} to ${pub.uri}`)
+
+              return {
+                pub: pub,
+                status: status
+              }
+            })
           }
-        })
+        }
+
+        this.Connection.open()
       }
-
-      // If array subs exists, subscribe to each
-      // console.log(pubArray.length)
-      if (pubArray.length) {
-        this.pubStats = pubArray.map(pub => {
-          let status = true
-
-          if (!Array.isArray(pub.data)) {
-            pub.data = [pub.data]
-          }
-
-          try {
-            session.publish(pub.uri, [pub.data], {data: pub.data})
-          } catch(e) {
-            status = e
-          }
-
-          // console.log(`Published ${pub.data} to ${pub.uri}`)
-
-          return {
-            pub: pub,
-            status: status
-          }
-        })
-      }
-    }
-
-    // Open Connection
-    this.Connection.open()
+    })
   }
 
   // Close connection
   close() {
-    this.Connection.close()
+    NetInfo.isConnected.fetch().then(isConnected => {
+      if (isConnected) this.Connection.close()
+    })
   }
 
   // Listens for connection close
