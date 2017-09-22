ed6de0d578678b524f7f937930e7aeec931384e3
yannunes <yannunes@gmail.com>
2017-09-22 03:53:12 -0300

    Add getDuration function to helper class

--- App/Helpers/index.js
+++ App/Helpers/index.js
@@ -1,5 +1,3 @@
-
-
 export function groupBy (dataSet, field) {
   const collectionObject = dataSet.reduce((finalGroup, data) => {
     (finalGroup[data[field]] = finalGroup[data[field]] || []).push(data);
@@ -17,4 +15,23 @@ export function flatten(array) {
 
 export function capitalize(string) {
   return string[0].toUpperCase() + string.slice(1)
+}
+
+export function getHourFromTimeStamp (timeStamp) {
+  const time = new Date(timeStamp)
+  let hours = time.getHours() + 3 // Timezone offset. Brazil is GMT+3, hence 3
+  let minutes = time.getMinutes()
+  if (hours < 10) hours = `0${hours}`
+  if (minutes < 10) minutes = `0${minutes}`
+
+  return `${hours}:${minutes}`
+}
+
+export function getDurationFromEvent(event) {
+  if (!event.start || !event.end) return ''
+
+  const start = getHourFromTimeStamp(event.start)
+  const end = getHourFromTimeStamp(event.end)
+
+  return `${start} - ${end}`
 }
