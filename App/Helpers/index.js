02b4aad4d63fb23593fb258dfe476da75fb8fdc5
yannunes <yannunes@gmail.com>
2017-09-22 12:47:47 -0300

    Add h to duration

--- App/Helpers/index.js
+++ App/Helpers/index.js
@@ -24,7 +24,7 @@ export function getHourFromTimeStamp (timeStamp) {
   if (hours < 10) hours = `0${hours}`
   if (minutes < 10) minutes = `0${minutes}`
 
-  return `${hours}:${minutes}`
+  return `${hours}:${minutes}h`
 }
 
 export function getDurationFromEvent(event) {
