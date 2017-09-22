8dedf1589305fb2193d6a1e8ca34f19fc57a8075
yannunes <yannunes@gmail.com>
2017-09-21 23:20:29 -0300

    Change icon on click to favorite

--- App/Components/EventCard.js
+++ App/Components/EventCard.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { ScrollView, Text, Image, View, ListView, StyleSheet } from 'react-native'
+import {
+  ScrollView,
+  Text,
+  Image,
+  View,
+  ListView,
+  StyleSheet,
+  TouchableHighlight
+} from 'react-native'
 import { Fonts, Colors, Metrics } from '../Themes/'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
@@ -9,6 +17,14 @@ import styles from './Styles/EventCardStyles'
 
 // <View style={styles.}></View>
 export default class EventCard extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      isFavorite: false,
+    }
+  }
+
   getHourFromTimeStamp (timeStamp) {
     const time = new Date(timeStamp)
     let hours = time.getHours() + 3 // Timezone offset. Brazil is GMT+3, hence 3
@@ -30,52 +46,65 @@ export default class EventCard extends Component {
 
   shouldComponentUpdate (nextProps, nextState) {
     return (
+      nextState.isFavorite != this.state.isFavorite ||
       nextProps.event.name != this.props.event.name ||
       nextProps.event.eventType != this.props.event.eventType ||
       this.getDurantion(nextProps.event) != this.getDurantion(this.props.event)
     )
   }
 
+  getIcon() {
+    return this.state.isFavorite? 'check' : 'plus'
+  }
+
+  favoritePress() {
+    const nextState = !this.state.isFavorite
+    // console.log(nextState)
+    this.setState({
+      isFavorite: nextState
+    })
+  }
+
   render () {
-    // console.log(this.props.event)
     return (
-      <View style={styles.contentContainer}>
-        <View style={styles.name}>
-          <Text style={styles.nameText}>
-            {this.props.event.name}
-            </Text>
-        </View>
-        <View style={styles.eventType}>
-          <Text style={styles.eventTypeText}>
-            {this.props.event.eventType.split(' - ')[0] + ' ' + (this.props.event.order || '')}
-          </Text>
-        </View>
-        <View style={styles.timeContainer}>
-          <View style={styles.time}>
-
-            <View style={styles.timeStamp}>
-              <View style={styles.timeStampIconContainer}>
-                <Icon style={styles.timeStampIconStyle} name="clock-o" />
-              </View>
-              <View style={styles.timeTextContainer}>
-                <Text style={styles.timeTextStyle}>
-                  {this.getDurantion(this.props.event)}
-                </Text>
-              </View>
-
-            </View>
-
-            <View style={styles.favorite}>
-              <View style={styles.favoriteIconContainer}>
-                <Icon style={styles.timeStampIconStyle} name="plus" />
-              </View>
-            </View>
-
+        <View style={styles.contentContainer}>
+          <View style={styles.name}>
+            <Text style={styles.nameText}>
+              {this.props.event.name}
+              </Text>
           </View>
-          <View style={styles.like}></View>
-        </View>
+          <View style={styles.eventType}>
+            <Text style={styles.eventTypeText}>
+              {this.props.event.eventType.split(' - ')[0] + ' ' + (this.props.event.order || '')}
+            </Text>
+          </View>
+          <View style={styles.timeContainer}>
+            <View style={styles.time}>
 
-      </View>
+              <View style={styles.timeStamp}>
+                <View style={styles.timeStampIconContainer}>
+                  <Icon style={styles.timeStampIconStyle} name="clock-o" />
+                </View>
+                <View style={styles.timeTextContainer}>
+                  <Text style={styles.timeTextStyle}>
+                    {this.getDurantion(this.props.event)}
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.favorite}>
+                <View style={styles.favoriteIconContainer}>
+                  <Icon
+                    onPress={() => this.favoritePress()}
+                    style={styles.timeStampIconStyle}
+                    name={(() => this.getIcon())()}
+                  />
+                </View>
+              </View>
+            </View>
+            <View style={styles.like}></View>
+          </View>
+
+        </View>
     )
   }
 }
